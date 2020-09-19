1a54d59b82ee15bbd0178e865ed771a0bf5631b1
masidulhaq80 <70268450+masidulhaq80@users.noreply.github.com>
2020-09-19 15:52:22 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    margin:10,
+    nav:true,
+    center:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
